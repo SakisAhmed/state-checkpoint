2d4539c8554ef23863a518397a95f015bafdc442
Sakis Ahmed <sakis.ahmed.insat@gmail.com>
2021-11-08 22:30:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Badge } from 'react-bootstrap';
+import React from "react"
+class App extends React.Component{
+  state = {
+    Person:{fullName:"Sakis Ahmed",
+    bio:"Ingenieur Front End React JS",
+    imgSrc:"/Photo.png",
+    profession:"inge"
+    },
+    show:false,
+    compteur: 0
+  }
+   handleName=()=>{
+     this.setState({show:!this.state.show});
+     console.log(this.state.show);
+   }
 
-function App() {
-  return (
+
+   componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID); 
+  }
+
+  tick() {
+    this.setState({
+      compteur: this.state.compteur+1
+    });
+  }
+  render() {
+      
+  return ( this.state.show?(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Card style={{ width: '18rem',display:'flex', justifyContent: 'space-around'}}>
+        <Card.Img variant="top" src={this.state.Person.imgSrc}/>
+        <Card.Body>
+          <Card.Title>{this.state.Person.fullName}</Card.Title>
+          <Card.Text>
+            {this.state.Person.bio}<br/>
+            {this.state.Person.profession}
+          </Card.Text>
+          <Button variant="warning" onClick={()=>this.handleName()}>hide</Button>
+        </Card.Body>
+      </Card>
+      <Badge pill bg="secondary">Component Life: {this.state.compteur} seconds</Badge>
+    </div>)
+    :(
+      <div className="App">
+    <Button type="submit" variant="info" onClick={()=>this.handleName()} >Presentation</Button><br/>
+    <Badge pill bg="secondary">Component Life: {this.state.compteur} seconds</Badge>     
     </div>
+    )
   );
 }
-
+}
 export default App;
